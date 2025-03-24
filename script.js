381ce98555995c31f642e81795a24b4c1b4d9b02
flozilla2077 <200891785+flozilla2077@users.noreply.github.com>
2025-03-24 20:05:36 +0100

    logic in place for human input to be case insensitive
    logic in place to the game and ++ on the
    scores
    
    ready to make score and loop

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("hello, world")
-
 // play the round code
     // ??
 //computer choice code
@@ -22,30 +20,85 @@ function playRound(humanChoice, computerChoice) {
   
 //define Variables
 
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  let humanScore = 0;
-  let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+let gamewinner = getPlayRound(computerSelection, humanSelection);
+
   
-  function getComputerChoice()    {
+  function getComputerChoice(computerSelection)    {
     const computerChoiceNum = Math.random();
     console.log(computerChoiceNum);
-    if (computerChoiceNum <= 1/3) {
-        getComputerChoice = `Rock`;
-    } else if ( 1/3 < computerChoiceNum < 3/3) {
-        getComputerChoice = `Paper`;
+    if (computerChoiceNum < 1/3) {
+        computerSelection = `Rock`;
+    } else if ( 1/3 < computerChoiceNum && computerChoiceNum < 2/3) {
+        computerSelection = `Paper`;
     }  else {
-        getComputerChoice = `Scissors`;
+        computerSelection = `Scissors`;
     }
-    console.log(`computer says: ${getComputerChoice}`);
-    return getComputerChoice;
+    console.log(`computer says: ${computerSelection}`);
+    return computerSelection;
   }
+
   
-  function getHumanChoice(getHumanChoice)   {
-    getHumanChoice = prompt(`What is it going to be: rock, paper or scissors?`);
-    console.log(`human says: ${getHumanChoice}`);
+  function getHumanChoice(humanSelection)   {
+    humanSelection = prompt(`What is it going to be: rock, paper or scissors?`);
+    console.log(`human says: ${humanSelection}`);
+    return humanSelection.charAt(0).toUpperCase() + humanSelection.slice(1).toLowerCase();
   }
 
- /*  function playRound(, ) {
+  function getPlayRound(computerSelection, humanSelection) {
+    console.log(`the input for the comparison for the computer is "${computerSelection}" and for the human "${humanSelection}". Now we play ...`)
+    
+    // if human plays rock && computer plays rock, then humanscore +0 and computer score +0
 
-  } */
+    if (humanSelection === `Rock` && computerSelection === `Rock`)   {
+        console.log("tie");
+    }
+    // if human plays rock && computer plays papers, then humanscore +0 and computer score +1
+    else if (humanSelection === `Rock` && computerSelection === `Paper`)   {
+        console.log("Computer Wins");
+        ++computerScore;
+    }
+    // if human plays rock && computer plays scissors, then humanscore +1 and computer score +0
+    else if (humanSelection === `Rock` && computerSelection === `Scissors`)   {
+        console.log("Human Wins");
+        ++humanScore;
+    }
+    // if human plays paper && computer plays rock, then humanscore +1 and computer score +0
+    else if (humanSelection === `Paper` && computerSelection === `Rock`)   {
+        console.log("Human Wins");
+        ++humanScore;
+    }    // if human plays paper && computer plays paper, then humanscore +0 and computerscore +0
+    else if (humanSelection === `Paper` && computerSelection === `Paper`)   {
+        console.log("tie");
+    }
+    // if human plays paper && computer plays scissors, then human score +0 and computer score +1
+    else if (humanSelection === `Paper` && computerSelection === `Scissors`)   {
+        console.log("Computer Wins");
+        ++computerScore;
+    }
+    // if human plays scissors && computer plays rock, then human score +0 and computer score +1
+    else if (humanSelection === `Scissors` && computerSelection === `Rock`)   {
+        console.log("Computer Wins");
+        ++computerScore;
+    }
+    // if human plays scissors && computer plays paper, then human score +1 and computer score +0
+    else if (humanSelection === `Scissors` && computerSelection === `Paper`)   {
+        console.log("Human Wins");
+        ++humanScore;
+    }
+    // if human plays scissors && computer plays scissors, then human score +0 and computer score +0
+    else if (humanSelection === `Scissors` && computerSelection === `Scissors`)   {
+        console.log("tie");
+    }
+    // Else
+    else {
+        console.log("invalid input");
+    }
+
+    console.log(`the computer has ${computerScore} and the human has ${humanScore}`);
+
+} 
+
